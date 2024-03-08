77cc0cf77c97746ec6a72bf48679dc4ff92cf640
Attila Pinter <pinter@xund.ai>
2024-03-08 19:48:24 +0700

    fix: defaultSession to plasma

--- throtur/DE/plasma6.nix
+++ throtur/DE/plasma6.nix
@@ -5,7 +5,7 @@
 services.xserver.enable = true;
 services.xserver.displayManager.sddm.wayland.enable = true;
 services.xserver.desktopManager.plasma6.enable = true;
-services.xserver.displayManager.defaultSession = "plasmawayland";
+services.xserver.displayManager.defaultSession = "plasma";
 
 qt = {
   enable = true;
