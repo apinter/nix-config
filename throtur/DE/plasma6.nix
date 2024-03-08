efe98e0458e85fcde1b3b0b793d65a8528212e45
Attila Pinter <adathor@opensuse.org>
2024-03-08 22:00:27 +0700

    test: remove sddm settings

--- throtur/DE/plasma6.nix
+++ throtur/DE/plasma6.nix
@@ -3,8 +3,8 @@
 {
 
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.theme = "breeze";
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.displayManager.sddm.theme = "breeze";
   services.xserver.displayManager.sddm.wayland.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
   services.xserver.displayManager.defaultSession = "plasma";
