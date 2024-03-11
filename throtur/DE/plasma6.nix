24c1051238210a15faca0e76f3c90d5d9853b99f
Attila Pinter <adathor@opensuse.org>
2024-03-11 09:14:47 +0700

    fix: portal and kde packages

--- throtur/DE/plasma6.nix
+++ throtur/DE/plasma6.nix
@@ -2,10 +2,10 @@
 
 {
   xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-kde ];
+  xdg.portal.extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
 
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.theme = "breeze";
   services.xserver.displayManager.sddm.wayland.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
