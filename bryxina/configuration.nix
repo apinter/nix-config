c8b1c1f49b4f3cdacfee90ca40c447ac96f183ca
Attila Pinter <adathor@opensuse.org>
2024-03-30 14:27:35 +0700

    chore: update ssh config, add new keys

--- bryxina/configuration.nix
+++ bryxina/configuration.nix
@@ -46,7 +46,10 @@
     home = "/home/apinter";
     description = "Attila Pinter";
     extraGroups = [ "wheel" "devops" ];
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICACL0UUvhrkRty9z3mv629Qg+v3AVt6G8mlMVj9iBE/ Attila Pinter" ];
+    openssh.authorizedKeys.keys = [ 
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINYgL/PMWtjixH8gzkXuuU03GcgdXFNXfX42HuFGGoHGAAAABHNzaDo= tw.kazeshini-30-03-2024-adathor-yubikeyA" 
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEGr9vLSNBrHSY2RwFHpkXWSCGPtvRqxgVLKduww+1FAAAAABHNzaDo= tw.kazeshini-30-03-2024-adathor-yubikeyC" 
+    ];
   };
 
   security.sudo.extraRules = [
@@ -91,7 +94,12 @@
   hardware.opengl.driSupport32Bit = true;
   programs.dconf.enable = true;
 
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+    settings.PermitRootLogin = "no";
+  };
   hardware.sane.enable = true;
   hardware.sane.extraBackends = [ pkgs.sane-backends ];
 
