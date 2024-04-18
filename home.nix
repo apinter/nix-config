bba1ea9050de2fae888250e1c49f99abe72561d9
Attila Pinter <adathor@opensuse.org>
2024-04-18 13:54:34 +0700

    test: install hello world

--- home.nix
+++ home.nix
@@ -4,6 +4,9 @@
   home.username = "apinter";
   home.homeDirectory = "/home/apinter";
   home.stateVersion = "23.11";
+  home.packages = [
+    pkgs.hello
+  ];
 
   home.sessionVariables = {
     EDITOR = "vim";
