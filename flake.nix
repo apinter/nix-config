f1627d6a653663ed5247e14f91112735e9e7c4de
Attila Pinter <adathor@opensuse.org>
2024-04-18 12:53:14 +0700

    fix: set correct user

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.jdoe = import ./throtur/home.nix;
+            home-manager.users.apinter = import ./throtur/home.nix;
           }
         ];
         };
