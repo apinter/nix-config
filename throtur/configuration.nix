48059873ee87fa75a15c036df4ef5280ff27704c
Attila Pinter <adathor@opensuse.org>
2024-04-22 06:41:39 +0700

    update: added user to podman group

--- throtur/configuration.nix
+++ throtur/configuration.nix
@@ -50,7 +50,7 @@
     isNormalUser = true;
     home = "/home/apinter";
     description = "Attila Pinter";
-    extraGroups = [ "wheel" "devops" ];
+    extraGroups = [ "wheel" "devops" "podman" ];
     openssh.authorizedKeys.keys = [ 
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINYgL/PMWtjixH8gzkXuuU03GcgdXFNXfX42HuFGGoHGAAAABHNzaDo= tw.kazeshini-30-03-2024-adathor-yubikeyA" 
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEGr9vLSNBrHSY2RwFHpkXWSCGPtvRqxgVLKduww+1FAAAAABHNzaDo= tw.kazeshini-30-03-2024-adathor-yubikeyC" 
