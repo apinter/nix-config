9df264c09bc967aeb4eca50ffffaa590c40b6783
Attila Pinter <adathor@opensuse.org>
2024-05-07 02:25:57 +0700

    feat: added ara kube

--- busybee/containers.nix
+++ busybee/containers.nix
@@ -64,8 +64,7 @@ systemd.user.services.jellyfin = {
         "-${pkgs.podman}/bin/podman pull --authfile=/home/apinter/.secret/auth.json docker.io/jellyfin/jellyfin:latest"
         ];
         ExecStart = "${pkgs.podman}/bin/podman kube play /home/apinter/kube/jellyfin.yml";
-        ExecStop = "${pkgs.podman}/bin/podman pod stop jellyfin-pod";
-        ExecStopPost = "${pkgs.podman}/bin/podman pod rm jellyfin-pod";
+        ExecStop = "${pkgs.podman}/bin/podman kube down /home/apinter/kube/jellyfin.yml";
         RemainAfterExit = true;
     };
     wantedBy = [ "default.target" ];
@@ -102,12 +101,47 @@ systemd.user.services.homepage = {
         "-${pkgs.podman}/bin/podman pull --authfile=/home/apinter/.secret/auth.json registry.adathor.com/devops/homepage:latest"
         ];
         ExecStart = "${pkgs.podman}/bin/podman kube play /home/apinter/kube/homepage.yml";
-        ExecStop = "${pkgs.podman}/bin/podman pod stop homepage-pod";
-        ExecStopPost = "${pkgs.podman}/bin/podman pod rm homepage-pod";
+        ExecStop = "${pkgs.podman}/bin/podman kube down /home/apinter/kube/homepage.yml";
         RemainAfterExit = true;
     };
     wantedBy = [ "default.target" ];
 };
 
+systemd.user.services.ara = {
+    enable = true;
+    description = "ara-pod";
+    after = [ "network-online.target" "basic.target" ];
+    environment = {
+        HOME = "/home/apinter";
+        LANG = "en_US.UTF-8";
+        USER = "apinter";
+    };
+    path = [ 
+        "/run/wrappers"
+        pkgs.podman
+        pkgs.bash
+        pkgs.conmon
+        pkgs.crun
+        pkgs.slirp4netns
+        pkgs.su
+        pkgs.shadow
+        pkgs.fuse-overlayfs
+        config.virtualisation.podman.package
+    ];
+    unitConfig = {
+    };
+    serviceConfig = {
+        Type = "oneshot";
+        TimeoutStartSec = 900;
+        ExecStartPre = lib.mkBefore [
+        "-${pkgs.podman}/bin/podman pod rm ara-api-pod"
+        "-${pkgs.podman}/bin/podman pull --authfile=/home/apinter/.secret/auth.json quay.io/recordsansible/ara-api:latest"
+        ];
+        ExecStart = "${pkgs.podman}/bin/podman kube play /home/apinter/kube/ara.yml";
+        ExecStop = "${pkgs.podman}/bin/podman kube down /home/apinter/kube/ara.yml";
+        RemainAfterExit = true;
+    };
+    wantedBy = [ "default.target" ];
+};
 
 }
