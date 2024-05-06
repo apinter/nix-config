5b43e72406771b99d00dc9b34fd7ecdccb1389f7
Attila Pinter <adathor@opensuse.org>
2024-05-07 01:09:11 +0700

    fix: remove opengl packages, switch to nixos-hardware

--- otong/hardware/accel.nix
+++ otong/hardware/accel.nix
@@ -6,11 +6,5 @@
   };
   hardware.opengl = {
     enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
   };
 }
