89701cc236d1a90a9dd217a382d80e581c1bb535
Attila Pinter <adathor@opensuse.org>
2024-05-07 06:01:55 +0700

    feat: added monitoring, hedgedoc, matrix, gitea

--- busybee/containers.nix
+++ busybee/containers.nix
@@ -144,4 +144,154 @@ systemd.user.services.ara = {
     wantedBy = [ "default.target" ];
 };
 
+systemd.user.services.hedgedoc = {
+    enable = true;
+    description = "hedgedoc-pod";
+    after = [ "network-online.target" "basic.target" ];
+    environment = {
+        HOME = "/home/apinter";
+        LANG = "en_US.UTF-8";
+        USER = "apinter";
+    };
+    path = [ 
+        "/run/wrappers"
+        pkgs.podman
+        pkgs.bash
+        pkgs.conmon
+        pkgs.crun
+        pkgs.slirp4netns
+        pkgs.su
+        pkgs.shadow
+        pkgs.fuse-overlayfs
+        config.virtualisation.podman.package
+    ];
+    unitConfig = {
+    };
+    serviceConfig = {
+        Type = "oneshot";
+        TimeoutStartSec = 900;
+        ExecStartPre = lib.mkBefore [
+        "-${pkgs.podman}/bin/podman pod rm hedgedoc"
+        "-${pkgs.podman}/bin/podman pull --authfile=/home/apinter/.secret/auth.json docker.io/library/postgres:13.4-alpine"
+        "-${pkgs.podman}/bin/podman pull --authfile=/home/apinter/.secret/auth.json quay.io/hedgedoc/hedgedoc:1.9.9-alpine"
+        ];
+        ExecStart = "${pkgs.podman}/bin/podman kube play /home/apinter/kube/hedgedoc.yml";
+        ExecStop = "${pkgs.podman}/bin/podman kube down /home/apinter/kube/hedgedoc.yml";
+        RemainAfterExit = true;
+    };
+    wantedBy = [ "default.target" ];
+};
+
+systemd.user.services.parallel = {
+    enable = true;
+    description = "parallel-pod";
+    after = [ "network-online.target" "basic.target" ];
+    environment = {
+        HOME = "/home/apinter";
+        LANG = "en_US.UTF-8";
+        USER = "apinter";
+    };
+    path = [ 
+        "/run/wrappers"
+        pkgs.podman
+        pkgs.bash
+        pkgs.conmon
+        pkgs.crun
+        pkgs.slirp4netns
+        pkgs.su
+        pkgs.shadow
+        pkgs.fuse-overlayfs
+        config.virtualisation.podman.package
+    ];
+    unitConfig = {
+    };
+    serviceConfig = {
+        Type = "oneshot";
+        TimeoutStartSec = 900;
+        ExecStartPre = lib.mkBefore [
+        "-${pkgs.podman}/bin/podman pod rm parallel-pod"
+        "-${pkgs.podman}/bin/podman pull --authfile=/home/apinter/.secret/auth.json docker.io/matrixdotorg/synapse:latest"
+        ];
+        ExecStart = "${pkgs.podman}/bin/podman kube play /home/apinter/kube/parallel.yml";
+        ExecStop = "${pkgs.podman}/bin/podman kube down /home/apinter/kube/parallel.yml";
+        RemainAfterExit = true;
+    };
+    wantedBy = [ "default.target" ];
+};
+
+systemd.user.services.monitoring = {
+    enable = true;
+    description = "monitoring-pod";
+    after = [ "network-online.target" "basic.target" ];
+    environment = {
+        HOME = "/home/apinter";
+        LANG = "en_US.UTF-8";
+        USER = "apinter";
+    };
+    path = [ 
+        "/run/wrappers"
+        pkgs.podman
+        pkgs.bash
+        pkgs.conmon
+        pkgs.crun
+        pkgs.slirp4netns
+        pkgs.su
+        pkgs.shadow
+        pkgs.fuse-overlayfs
+        config.virtualisation.podman.package
+    ];
+    unitConfig = {
+    };
+    serviceConfig = {
+        Type = "oneshot";
+        TimeoutStartSec = 900;
+        ExecStartPre = lib.mkBefore [
+        "-${pkgs.podman}/bin/podman pod rm monitoring"
+        "-${pkgs.podman}/bin/podman pull --authfile=/home/apinter/.secret/auth.json docker.io/matrixdotorg/synapse:latest"
+        ];
+        ExecStart = "${pkgs.podman}/bin/podman kube play /home/apinter/kube/monitoring.yml";
+        ExecStop = "${pkgs.podman}/bin/podman kube down /home/apinter/kube/monitoring.yml";
+        RemainAfterExit = true;
+    };
+    wantedBy = [ "default.target" ];
+};
+
+systemd.user.services.gitea = {
+    enable = true;
+    description = "gitea-pod";
+    after = [ "network-online.target" "basic.target" ];
+    environment = {
+        HOME = "/home/apinter";
+        LANG = "en_US.UTF-8";
+        USER = "apinter";
+    };
+    path = [ 
+        "/run/wrappers"
+        pkgs.podman
+        pkgs.bash
+        pkgs.conmon
+        pkgs.crun
+        pkgs.slirp4netns
+        pkgs.su
+        pkgs.shadow
+        pkgs.fuse-overlayfs
+        config.virtualisation.podman.package
+    ];
+    unitConfig = {
+    };
+    serviceConfig = {
+        Type = "oneshot";
+        TimeoutStartSec = 900;
+        ExecStartPre = lib.mkBefore [
+        "-${pkgs.podman}/bin/podman pod rm gitea"
+        "-${pkgs.podman}/bin/podman pull --authfile=/home/apinter/.secret/auth.json docker.io/library/postgres:14"
+        "-${pkgs.podman}/bin/podman pull --authfile=/home/apinter/.secret/auth.json docker.io/gitea/gitea:latest-rootless"
+        ];
+        ExecStart = "${pkgs.podman}/bin/podman kube play /home/apinter/kube/gitea.yml";
+        ExecStop = "${pkgs.podman}/bin/podman kube down /home/apinter/kube/gitea.yml";
+        RemainAfterExit = true;
+    };
+    wantedBy = [ "default.target" ];
+};
+
 }
