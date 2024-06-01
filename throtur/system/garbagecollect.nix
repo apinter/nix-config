82ccc052c7416d926ad0e77da4184c7faf612a57
Attila Pinter <adathor@opensuse.org>
2024-06-01 09:58:01 +0700

    chore: change gc freq to daily

--- throtur/system/garbagecollect.nix
+++ throtur/system/garbagecollect.nix
@@ -3,7 +3,7 @@
 {
   nix.gc.automatic = true;
   nix.gc.persistent = true;
-  nix.gc.dates = "weekly";
+  nix.gc.dates = "daily";
   nix.gc.options = "--delete-older-than 5d";
   nix.gc.randomizedDelaySec = "30m";
 }
