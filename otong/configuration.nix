ae9bc61622cab809ed76ded6f5a945215ee87a62
Attila Pinter <adathor@opensuse.org>
2024-09-22 15:24:28 +0700

    chore: enable steam hw

--- otong/configuration.nix
+++ otong/configuration.nix
@@ -111,4 +111,5 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;  
   system.stateVersion = "23.05";
+  hardware.steam-hardware.enable = true;
 }
