34f6af3132e4aea38212a9540e8a867ea7b68117
Attila Pinter <adathor@opensuse.org>
2024-12-26 17:26:47 +0700

    ci: test GITHUB_TOKEN

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
   description = "Adathor's flake - just manages my fleet";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05"; 
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    nixpkgs.url = "nixpkgs/nixos-24.11"; 
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
