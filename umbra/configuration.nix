d81be919b60cd9a066b88963dedf7a73454972d6
Attila Pinter <adathor@opensuse.org>
2025-02-01 01:43:19 +0700

    fix: remove dup conf
    Signed-off-by: Attila Pinter <adathor@opensuse.org>

--- umbra/configuration.nix
+++ umbra/configuration.nix
@@ -64,7 +64,6 @@
     wrapperFeatures.gtk = true;
   };
   programs.light.enable = true;
-  zramSwap.enable = true;
   programs.sway.extraSessionCommands = ''
     export GNOME_KEYRING_CONTROL=/run/user/$UID/keyring
     export SSH_AUTH_SOCK=/run/user/$UID/keyring/ssh
