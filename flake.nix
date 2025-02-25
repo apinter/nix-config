90859d807f8b952a2f5cba33431aa8fd4181938f
Attila Pinter <adathor@opensuse.org>
2025-02-25 17:57:02 +0700

    fix: remove unused modules
    Signed-off-by: Attila Pinter <adathor@opensuse.org>

--- flake.nix
+++ flake.nix
@@ -4,40 +4,20 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.11"; 
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = {self, nixpkgs, nixos-hardware, home-manager, ...}:
+  outputs = {self, nixpkgs, nixos-hardware, ...}:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
-      umbra = lib.nixosSystem {
-        inherit system;
-        modules = [ ./umbra/configuration.nix ];
-        };
       brenda = lib.nixosSystem {
         inherit system;
         modules = [ ./brenda/configuration.nix ];
         };
-      sofie = lib.nixosSystem {
-        inherit system;
-        modules = [ ./sofie/configuration.nix ];
-        };
-      crate = lib.nixosSystem {
-        inherit system;
-        modules = [ ./crate/configuration.nix ];
-        };
       };
-    homeConfigurations = {
-      apinter = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ ./umbra/home.nix ];
-      };
-    };
   };
 }
