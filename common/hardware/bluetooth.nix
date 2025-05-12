a1c08c0e2274956ed4d0f9fc1951dec377737bf7
Attila Pinter <adathor@opensuse.org>
2025-05-12 08:40:36 +0700

    feat: merge bluetooth testing conf

--- common/hardware/bluetooth.nix
+++ common/hardware/bluetooth.nix
@@ -3,5 +3,34 @@
 {
   services.blueman.enable = true;
   hardware.bluetooth.enable = true;
+  hardware.enableAllFirmware = true;
+
+  services.pipewire.wireplumber ={
+    enable = true;
+    extraConfig.bluetoothEnhancements = {
+      "monitor.bluez.properties" = {
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.roles" = [ "a2dp_sink" "a2dp_source" "bap_sink" "bap_source" "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      "bluez5.codecs" = [ "sbc" "sbc_xq" "aac" ];
+      # "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      };
+    };
+};
+
+systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+};
+
+hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+      Experimental = true;
+      };
+  };
 
 }
