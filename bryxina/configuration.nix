5d9ed1e4ffac2f7be52ff9ddeb41006ea21a5b91
Attila Pinter <adathor@opensuse.org>
2025-05-13 13:41:27 +0700

    feat: move to sway

--- bryxina/configuration.nix
+++ bryxina/configuration.nix
@@ -9,7 +9,8 @@
       ../common/hardware/accel.nix
       ../common/system/garbagecollect.nix
       ../common/system/autoupgrade.nix
-      ../common/DE/plasma6.nix
+      # ../common/DE/plasma6.nix
+      ../common/DE/sway.nix
       ../common/hardware/fwupd.nix
       ../common/system/journald.nix
       ../common/networking/ssh.nix
