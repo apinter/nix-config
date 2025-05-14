b14e9bb15bfa0854277c21d04c00af281a4dc912
Attila Pinter <adathor@opensuse.org>
2025-05-14 13:19:30 +0700

    fix: greetd :/

--- common/DE/greetd.nix
+++ common/DE/greetd.nix
@@ -1,13 +1,13 @@
 { config, pkgs, callPackage, ... }:
 
 {
-
-  enable = true;                                                         
-  settings = {                                                           
-    default_session = {                                                  
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
-      user = "greeter";                                                  
-    };                                                                   
-  };                                                                     
-
+  services.greetd = {
+    enable = true;                                                         
+    settings = {                                                           
+      default_session = {                                                  
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        user = "greeter";                                                  
+      };                                                                   
+    };                                                                     
+  };
 }
