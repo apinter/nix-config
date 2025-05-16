baaefe44632f3d0625db2ef4f1aa858442361489
Attila Pinter <adathor@opensuse.org>
2025-05-16 08:14:41 +0700

    chore: enable autoprune

--- common/system/podman.nix
+++ common/system/podman.nix
@@ -4,6 +4,9 @@
   virtualisation = {
     podman = {
       enable = true;
+      autoPrune = {
+        enable = true;
+      };
       defaultNetwork.settings = {
         dns_enabled = true;
       };
