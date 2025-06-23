969a40aff1f7db523529f5e1987959878c913d32
Attila Pinter <adathor@opensuse.org>
2025-06-24 00:23:32 +0700

    chore: adjust gc

--- common/system/garbagecollect.nix
+++ common/system/garbagecollect.nix
@@ -4,6 +4,6 @@
   nix.gc.automatic = true;
   nix.gc.persistent = true;
   nix.gc.dates = "daily";
-  nix.gc.options = "--delete-older-than 15d";
+  nix.gc.options = "--delete-older-than 7d";
   nix.gc.randomizedDelaySec = "30m";
 }
