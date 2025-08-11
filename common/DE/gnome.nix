dacbf9ee37aebecb47ccb897b238562bb531c96d
Attila Pinter <adathor@protonmail.com>
2025-08-11 15:45:02 +0700

    test: remove mutter stuff

--- common/DE/gnome.nix
+++ common/DE/gnome.nix
@@ -19,21 +19,6 @@
     gnomeExtensions.appindicator
   ];
 
-  nixpkgs.overlays = [
-    # GNOME 46: triple-buffering-v4-46
-    (final: prev: {
-      mutter = prev.mutter.overrideAttrs (old: {
-        src = pkgs.fetchFromGitLab  {
-          domain = "gitlab.gnome.org";
-          owner = "vanvugt";
-          repo = "mutter";
-          rev = "triple-buffering-v4-46";
-          hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
-        };
-      });
-    })
-  ];
-
   environment.gnome.excludePackages = (with pkgs; [
     atomix # puzzle game
     cheese # webcam tool
