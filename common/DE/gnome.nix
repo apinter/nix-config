a941b748ba1ed16e681c0bf9259ec05bb16abe25
Attila Pinter <adathor@protonmail.com>
2025-08-11 16:23:43 +0700

    chore: disable triple buffer

--- common/DE/gnome.nix
+++ common/DE/gnome.nix
@@ -19,20 +19,20 @@
     gnomeExtensions.appindicator
   ];
 
-  nixpkgs.overlays = [
-    # GNOME 46: triple-buffering-v4-46
-    (final: prev: {
-      mutter = prev.mutter.overrideAttrs (old: {
-        src = pkgs.fetchFromGitLab  {
-          domain = "gitlab.gnome.org";
-          owner = "vanvugt";
-          repo = "mutter";
-          rev = "triple-buffering-v4-46";
-          hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
-        };
-      });
-    })
-  ];
+  # nixpkgs.overlays = [
+  #   # GNOME 46: triple-buffering-v4-46
+  #   (final: prev: {
+  #     mutter = prev.mutter.overrideAttrs (old: {
+  #       src = pkgs.fetchFromGitLab  {
+  #         domain = "gitlab.gnome.org";
+  #         owner = "vanvugt";
+  #         repo = "mutter";
+  #         rev = "triple-buffering-v4-46";
+  #         hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
+  #       };
+  #     });
+  #   })
+  # ];
 
   environment.gnome.excludePackages = (with pkgs; [
     atomix # puzzle game
