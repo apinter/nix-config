3a697cd033c14fe87a2f2e7dc140f9e515a4f331
Attila Pinter <adathor@protonmail.com>
2025-08-11 15:56:48 +0700

    test: maaaaannnyy.....

--- common/DE/gnome.nix
+++ common/DE/gnome.nix
@@ -4,7 +4,7 @@
   services = {
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
-  }  
+  };  
 
   services.udev = {
     enable = true;
@@ -17,6 +17,21 @@
   environment.systemPackages = with pkgs; [
     gnome.adwaita-icon-theme
     gnomeExtensions.appindicator
+  ];
+
+  nixpkgs.overlays = [
+    # GNOME 46: triple-buffering-v4-46
+    (final: prev: {
+      mutter = prev.mutter.overrideAttrs (old: {
+        src = pkgs.fetchFromGitLab  {
+          domain = "gitlab.gnome.org";
+          owner = "vanvugt";
+          repo = "mutter";
+          rev = "triple-buffering-v4-46";
+          hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
+        };
+      });
+    })
   ];
 
   environment.gnome.excludePackages = (with pkgs; [
