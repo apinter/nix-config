ccb019ab23670c64c00aaab25e8eb03b91c5cd4b
Attila Pinter <adathor@opensuse.org>
2025-09-09 07:40:57 +0700

    test: extend subgid/subuid ranges for homed user

--- common/system/homed.nix
+++ common/system/homed.nix
@@ -13,4 +13,17 @@
     enable = true;
     enableSSHSupport = true;
   };
+
+  users.users.adathor.subUidRanges = [
+  {
+    count = 165534;
+    startUid = 100001;
+    };
+  ];
+  users.users.adathor.subGidRanges = [
+  {
+    count = 165534;
+    startGid = 100001;
+    };
+  ];
 }
