f9bf9f674cdef5ded3114be8c940db45d528027c
Attila Pinter <adathor@opensuse.org>
2025-11-19 05:53:17 +0700

    chore: enable default fonts

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -2,23 +2,24 @@
 
 {
   fonts = {
+    enableDefaultPackages = true;
     packages =  with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-color-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-    liberation_ttf
-    hack-font
-    google-fonts
-    roboto
-    roboto-mono
-    ubuntu-sans
-    ubuntu-classic
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
+      liberation_ttf
+      hack-font
+      google-fonts
+      roboto
+      roboto-mono
+      ubuntu-sans
+      ubuntu-classic
     ];
   };
 }
