242d4b4c47ab8097f86a79fa27c3c3d2cf7a1323
Attila Pinter <adathor@opensuse.org>
2025-12-09 18:09:01 +0700

    chore: install amd cpu specific modules

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
         };
         modules = [
           ./bryxina/configuration.nix 
-          nixos-hardware.nixosModules.common-cpu-intel
+          nixos-hardware.nixosModules.common-cpu-amd
           nixos-hardware.nixosModules.common-pc-ssd
           ];
         };
