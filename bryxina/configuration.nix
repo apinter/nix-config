d2e2bc7793489afbe128ad3890955ab40a112b85
Attila Pinter <adathor@opensuse.org>
2025-12-19 20:27:08 +0700

    test: create a symlink

--- bryxina/configuration.nix
+++ bryxina/configuration.nix
@@ -43,12 +43,14 @@
       fsType = "nfs";
   };
 
-  sops.defaultSopsFile = ../secrets/example.yaml;
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   # sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   sops.age.generateKey = true;
-  sops.secrets.example_key = {};
-
+  sops.secrets.binaryters = {
+    sopsFile = ../secrets/binary.ini;
+    path = "/home/apinter/.binaryters.ini";
+    owner = "apinter";
+  };
 
   services.borgbackup.jobs.main = {
     paths = "/home/bryxina";
