2b586c8dfa76165ac24101b818dbbfb6a6ee3cc1
Attila Pinter <adathor@opensuse.org>
2026-02-03 07:49:33 +0700

    test: fix pipewire issues

--- modules/system/pipewire.nix
+++ modules/system/pipewire.nix
@@ -1,11 +1,15 @@
 { config, pkgs, callPackage, ... }:
 
 {
+
   services.pipewire = {
     enable = true;
-    # alsa.enable = true;
-    # alsa.support32Bit = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
   };
 
+  environment.systemPackages = with pkgs; [
+    pkgs.pavucontrol
+  ];
 }
