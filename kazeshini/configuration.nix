08c9b53f9a01de27fafd46f1b6e9eb87cd3d2154
Attila Pinter <adathor@opensuse.org>
2026-02-13 07:03:35 +0700

    feat: move to borg from pika

--- kazeshini/configuration.nix
+++ kazeshini/configuration.nix
@@ -23,7 +23,7 @@
       ../modules/system/kernel.nix
       ../modules/system/pipewire.nix
       ../modules/system/systemd-boot.nix
-      # ../modules/system/zram.nix
+      ../modules/system/zram.nix
       ../modules/system/rtkit.nix
       ../modules/networking/fw_off.nix
       ../modules/networking/tailscale.nix
@@ -80,6 +80,60 @@
     enable = true;                                                          
     xkb.options = "caps:swapescape";
   };                                                                           
+
+  services.borgbackup.jobs.main = {
+    startAt = "daily";
+    paths = [
+      "/.snapshots/HOME-SNAPSHOT"
+    ];
+    encryption = {
+      mode = "repokey-blake2";
+      passCommand = "cat /root/.secrets/borg_keyfile";
+    };
+    repo = "/home/apinter/Backup";
+    compression = "auto,zstd";
+    inhibitsSleep = true;
+    persistentTimer = true;
+    readWritePaths = [
+      "/.snapshots"
+    ];
+    extraCreateArgs = [
+      "--progress"
+      "--stats"
+    ];
+
+    prune.keep = {
+      daily = 7;
+      weekly = 4;
+      monthly = 12;
+      yearly = -1;
+    };
+
+    preHook = ''
+      if [ ! -d "/.snapshots" ]; then
+        ${pkgs.btrfs-progs}/bin/btrfs subvolume create /.snapshots
+      fi
+
+      if [ -d "/.snapshots/HOME-SNAPSHOT" ]; then
+        ${pkgs.btrfs-progs}/bin/btrfs subvolume delete /.snapshots/HOME-SNAPSHOT
+      fi
+
+      ${pkgs.btrfs-progs}/bin/btrfs subvolume snapshot -r /home /.snapshots/HOME-SNAPSHOT
+    '';
+
+    postHook = ''
+      source /opt/mtx/mtx.env
+
+        if [ "$exitStatus" -eq 0 ]; then
+          borg_status_msg="✅ Success"
+        else
+          borg_status_msg="❌ Failed"
+        fi
+
+      ${pkgs.btrfs-progs}/bin/btrfs subvolume delete /.snapshots/HOME-SNAPSHOT
+      ${pkgs.curl}/bin/curl -X PUT "https://matrix.adathor.com/_matrix/client/r0/rooms/$MY_MTX_ROOMID/send/m.room.message/$(date +%s)?access_token=$MY_MTX_TOKEN" -H "Content-Type: application/json" --data "{\"msgtype\":\"m.text\",\"body\":\"$HOSTNAME backup status is: $borg_status_msg \"}"
+    '';
+  };
 
   system.stateVersion = "25.05";
   hardware.steam-hardware.enable = true;
