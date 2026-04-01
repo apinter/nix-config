346cae117590046b7cfd6378b1c038ad1006f469
Attila Pinter <adathor@opensuse.org>
2026-04-01 10:44:19 +0700

    test: switch to mainline kernel

--- workstations/kazeshini/configuration.nix
+++ workstations/kazeshini/configuration.nix
@@ -20,7 +20,7 @@
       ../../modules/system/btrfs.nix
       ../../modules/system/docker.nix
       ../../modules/system/podman.nix
-      ../../modules/system/kernel.nix
+      # ../../modules/system/kernel.nix
       ../../modules/system/pipewire.nix
       ../../modules/system/systemd-boot.nix
       ../../modules/system/zram.nix
@@ -37,6 +37,8 @@
       ../../modules/system/cockpit.nix
       ./hardware-configuration.nix
     ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "kazeshini";
 
