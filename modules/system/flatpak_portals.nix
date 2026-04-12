f7a3c208fbfe6a7eec2e7910ac259d7c4722392d
Attila Pinter <adathor@protonmail.com>
2026-04-12 11:36:22 +0700

    chore: restore version

--- modules/system/flatpak_portals.nix
+++ modules/system/flatpak_portals.nix
@@ -1,18 +1,9 @@
 { config, pkgs, callPackage, ... }:
 
-let
-  pkgs_flatpak = import (pkgs.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "719c0c76fdadab54edb5704bbf9b939570cabbfb";
-    hash = "sha256-HdqLaB+u1cQEk9s95P4nWdHP+QAXFkXeAkMG+FdT9m8="; 
-  }) { inherit (pkgs) system; };
-in
-
 {
   programs.dconf.enable = true;
   services.flatpak.enable = true;
-  services.flatpak.package = pkgs_flatpak.flatpak; 
+
   xdg = {
     portal = {
       enable = true;
