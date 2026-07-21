e7a3519ed4ba4126cc069f267707e2a73088f5fb
Attila Pinter <adathor@opensuse.org>
2026-07-21 08:40:08 +0700

    fix: disable handy in ws config

--- workstations/kazeshini/configuration.nix
+++ workstations/kazeshini/configuration.nix
@@ -90,7 +90,7 @@
     enable = true;
     enableSSHSupport = true;
   };
-  programs.handy.enable = true;
+  # programs.handy.enable = true;
 
   services.borgbackup.jobs.main = {
     startAt = "*-*-* 11:00:00";
