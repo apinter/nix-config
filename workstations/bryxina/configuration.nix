2c8a88d761a9c46cea6ae68513584adfcf2e4145
Attila Pinter <adathor@opensuse.org>
2026-08-14 10:16:39 +0700

    chore: add new disk

--- workstations/bryxina/configuration.nix
+++ workstations/bryxina/configuration.nix
@@ -47,6 +47,17 @@
       fsType = "nfs";
   };
 
+  fileSystems."/home/bryxina/Steam" = {
+      device = "/dev/disk/by-label/DATA";
+      fsType = "btrfs";
+      options = [ "subvol=STEAM" "compress=zstd:1" ];
+  };
+
+  fileSystems."/home/bryxina/Store" = {
+      device = "/dev/disk/by-label/DATA";
+      fsType = "btrfs";
+      options = [ "subvol=STORE" "compress=zstd:1" ];
+  };
   # sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   # # sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   # sops.age.generateKey = true;
